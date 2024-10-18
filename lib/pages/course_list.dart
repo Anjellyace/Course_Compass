1efb91b89490acf2e0bf3070ff7c73a070668de1
lelemnq <gaylerflorencio@gmail.com>
2024-10-18 14:53:57 +0800

    updated

--- lib/pages/course_list.dart
+++ lib/pages/course_list.dart
@@ -13,9 +13,16 @@ class _CourseListState extends State<CourseList> {
 
   @override
   Widget build(BuildContext context) {
+    // Determine if the theme is dark
+    final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+    
+    // Set colors based on the theme
+    final textColor = isDarkMode ? Colors.white : Colors.black;
+    final courseContainerColor = isDarkMode ? Colors.grey[850] : Colors.grey[200];
+    
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:Color.fromARGB(255, 111, 190, 236),
+        backgroundColor: Color.fromARGB(255, 111, 190, 236),
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(26),
@@ -31,16 +38,22 @@ class _CourseListState extends State<CourseList> {
           ),
         ),
         centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous page
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height:30), // Space between AppBar and the text
-            const Text(
+            const SizedBox(height: 30), // Space between AppBar and the text
+            Text(
               'In that case, here are the best courses that\nalign with your interests!',
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700, color: textColor),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 20),
@@ -56,7 +69,7 @@ class _CourseListState extends State<CourseList> {
                   child: DropdownButtonHideUnderline(
                     child: DropdownButton<String>(
                       value: selectedLocation, // Bind selected value
-                      hint: const Text('Any Location'),
+                      hint: Text('Any Location', style: TextStyle(color: textColor)),
                       items: <String>[
                         'Makati City', 
                         'Pasig City', 
@@ -66,7 +79,7 @@ class _CourseListState extends State<CourseList> {
                       ].map((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
-                          child: Text(value),
+                          child: Text(value, style: TextStyle(color: textColor)),
                         );
                       }).toList(),
                       onChanged: (String? newValue) {
@@ -74,6 +87,7 @@ class _CourseListState extends State<CourseList> {
                           selectedLocation = newValue; // Update selected value
                         });
                       },
+                      dropdownColor: isDarkMode ? Colors.grey[800] : Colors.white, // Dropdown background
                     ),
                   ),
                 ),
@@ -84,11 +98,11 @@ class _CourseListState extends State<CourseList> {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    buildCourseContainer('Course 1', 'Location 1', 'University 1', context),
+                    buildCourseContainer('Course 1', 'Location 1', 'University 1', context, textColor, courseContainerColor),
                     const SizedBox(height: 10),
-                    buildCourseContainer('Course 2', 'Location 2', 'University 2', context),
+                    buildCourseContainer('Course 2', 'Location 2', 'University 2', context, textColor, courseContainerColor),
                     const SizedBox(height: 10),
-                    buildCourseContainer('Course 3', 'Location 3', 'University 3', context),
+                    buildCourseContainer('Course 3', 'Location 3', 'University 3', context, textColor, courseContainerColor),
                   ],
                 ),
               ),
@@ -100,7 +114,7 @@ class _CourseListState extends State<CourseList> {
   }
 
   // Function that builds a clickable container
-  Widget buildCourseContainer(String course, String location, String university, BuildContext context) {
+  Widget buildCourseContainer(String course, String location, String university, BuildContext context, Color textColor, Color? containerColor) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -111,20 +125,19 @@ class _CourseListState extends State<CourseList> {
       child: Container(
         padding: const EdgeInsets.all(16.0),
         decoration: BoxDecoration(
-          color: Colors.grey[200],
+          color: containerColor,
           borderRadius: BorderRadius.circular(15),
           border: Border.all(color: Colors.grey),
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(course, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text(location, style: const TextStyle(fontSize: 16)),
-            Text(university, style: const TextStyle(fontSize: 16)),
+            Text(course, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: textColor)),
+            Text(location, style: TextStyle(fontSize: 16, color: textColor)),
+            Text(university, style: TextStyle(fontSize: 16, color: textColor)),
           ],
         ),
       ),
     );
   }
 }
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,90 @@
 import 'package:course_compass/pages/interest_page.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(MyApp());
+  runApp(ThemeToggleWrapper());
+}
+
+class ThemeToggleWrapper extends StatefulWidget {
+  @override
+  _ThemeToggleWrapperState createState() => _ThemeToggleWrapperState();
+}
+
+class _ThemeToggleWrapperState extends State<ThemeToggleWrapper> {
+  bool _isDarkMode = false;
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Course Compass',
+      theme: _isDarkMode
+          ? ThemeData.dark().copyWith(
+              textTheme: ThemeData.dark().textTheme.apply(
+                    fontFamily: 'Times New Roman',
+                    bodyColor: Colors.white,
+                    displayColor: Colors.white,
+                  ),
+            )
+          : ThemeData(
+              primarySwatch: Colors.blue,
+              fontFamily: 'Times New Roman',
+              textTheme: ThemeData.light().textTheme.apply(
+                    bodyColor: Colors.black,
+                    displayColor: Colors.black,
+                  ),
+            ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 111, 190, 236),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(26),
+              bottomRight: Radius.circular(26),
+            ),
+          ),
+          title: Text(
+            'Course Compass',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            ThemeToggle(
+              isDarkMode: _isDarkMode,
+              onToggle: _toggleTheme,
+            ),
+          ],
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class ThemeToggle extends StatelessWidget {
+  final bool isDarkMode;
+  final VoidCallback onToggle;
+
+  ThemeToggle({required this.isDarkMode, required this.onToggle});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+      onPressed: onToggle,
+      color: Colors.white,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -43,82 +124,59 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor:Color.fromARGB(255, 111, 190, 236),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(26),
-            bottomRight: Radius.circular(26),
-          ),
-        ),
-        title: Text(
-          'Course Compass',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-        body: Padding(
-  padding: EdgeInsets.all(16.0),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        'What are your interests?',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w500,
-          color: Colors.black87,
-        ),
-      ),
-      SizedBox(height: 16),
-      Text(
-        'Select a course you\'re interested in:',
-        style: TextStyle(
-          fontSize: 16,
-          color: Colors.black54,
-        ),
-      ),
-      SizedBox(height: 20),
-      Wrap(
-        spacing: 8,
-        runSpacing: 8,
-        children: courses.map((course) {
-          return SizedBox(
-            width: (MediaQuery.of(context).size.width - 50) / 3, // Adjust for 3 buttons per row
-            height: 30, // Fixed height for all buttons
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: selectedCourse == course ? Colors.white : Colors.black,
-                backgroundColor: selectedCourse == course ? Color.fromARGB(255, 111, 190, 236) : Colors.grey[200],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: EdgeInsets.zero, // Remove default padding
-              ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> InterestPage(selectedCourse: course,),),);
-                setState(() {
-                  selectedCourse = course;
-                  
-                });
-              },
-              child: Text(
-                course,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16), // Smaller font size
-              ),
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'What are your interests?',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
             ),
-          );
-        }).toList(),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Select a course you\'re interested in:',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 20),
+          Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: courses.map((course) {
+              return SizedBox(
+                width: (MediaQuery.of(context).size.width - 50) / 3,
+                height: 30,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: selectedCourse == course ? Colors.white : Theme.of(context).textTheme.bodyLarge?.color,
+                    backgroundColor: selectedCourse == course ? Color.fromARGB(255, 111, 190, 236) : Theme.of(context).buttonTheme.colorScheme?.background,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: EdgeInsets.zero,
+                  ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> InterestPage(selectedCourse: course,),),);
+                    setState(() {
+                      selectedCourse = course;
+                    });
+                  },
+                  child: Text(
+                    course,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
-    ],
-  ),
-),
-);
+    );
   }
 }
